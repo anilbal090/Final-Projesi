285dfeac2f3baa375b906033cc9d85eb7a754fec
KUTLUAY <KUTLUAY@DESKTOP-28QCPLJ>
2018-01-18 23:48:08 +0200

    duzenleme

--- Proje/src/test/java/com/proje/Proje/ProjeApplicationTests.java
+++ Proje/src/test/java/com/proje/Proje/ProjeApplicationTests.java
@@ -1,5 +1,12 @@
 package com.proje.Proje;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +15,7 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,5 +37,48 @@ public class ProjeApplicationTests {
         assertThat(this.json.write(haber)).extractingJsonPathStringValue("@.haberIcerik")
                 .isEqualTo("Icerik");
     }
+  
+  @Test
+	public void testHaberYaz() throws Exception {
+	  	Haber haber = RESTServisi.haberYAZ("deneme", "icerik");
+	  	assertEquals(haber.getHaberBaslik(), "deneme");
+	  	assertEquals(haber.getHaberIcerik(), "icerik");
+	}
+  
+  @Test
+ 	public void testHaberListele() throws Exception{
+
+	  	RESTServisi.haberYAZ("Baslik", "icerik");
+	    assertTrue(RESTServisi.haberListele().stream().anyMatch(item -> "Baslik".equals(item.getHaberBaslik())));	  	
+ 	}
+  
+  @Test
+	public void testHaberTemizle() throws Exception{
+
+		assertTrue(RESTServisi.haberTemizle(), true);
+	}
+  
+  @Test
+ 	public void testHaberSil() throws Exception{
+	    List<Haber> HABERLERTEST = new ArrayList<>();
+	  	Haber haber = RESTServisi.haberYAZ("deneme", "icerik");
+	    HABERLERTEST.add(haber);
+	  	assertEquals(RESTServisi.haberSil(0),"OK");
+
+ 	}
+  
+  @Test
+ 	public void testClass() {
+ 		boolean hataOlustu = false;
+ 		try {
+ 			RESTServisi servis = new RESTServisi();
+ 		} catch(Exception e) {
+ 			hataOlustu = true;
+ 		}
+ 		
+ 		assertFalse(hataOlustu);
+ 	}
+  
+	
 
 }
